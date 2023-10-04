be369c0e75f47d6ebc46f7cb8f06f41ad82d134d
Jco <jesus14k02@outlook.es>
2023-10-03 19:10:22 -0500

    init inventory-management

--- employee-management/src/main/java/com/proyect/employee/employee/config/SwaggerConfig.java
+++ employee-management/src/main/java/com/proyect/employee/employee/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
