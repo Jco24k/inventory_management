48e4bc3cec381f13f8581b78ac8c877765a4f74b
Jco <jesus14k02@outlook.es>
2023-10-05 16:40:19 -0500

    init purchase-management

---
+++ purchase-management/src/test/java/com/purchase/management/PurchaseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purchase.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
