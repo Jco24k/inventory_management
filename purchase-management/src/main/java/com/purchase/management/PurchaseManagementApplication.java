55124112ea46764627100ac6904f0849b0c0b488
Jco <jesus14k02@outlook.es>
2023-10-09 13:11:51 -0500

    add eureka-service

--- purchase-management/src/main/java/com/purchase/management/PurchaseManagementApplication.java
+++ purchase-management/src/main/java/com/purchase/management/PurchaseManagementApplication.java
@@ -2,10 +2,12 @@ package com.purchase.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class PurchaseManagementApplication {
 
 	public static void main(String[] args) {
